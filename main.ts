e571cb6b82b8ecb59b237680b2ad8d79d4e66a2c
somethingsaurus7 <96009468+somethingsaurus7@users.noreply.github.com>
2022-02-12 08:55:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+lcd1602.setAddress3()
+lcd1602.set_backlight(lcd1602.on_off.on)
+lcd1602.clear()
+lcd1602.set_LCD_Show(lcd1602.visibled.visible)
+lcd1602.putString(
+"Hello",
+0,
+0
+)
 basic.forever(function () {
 	
 })
